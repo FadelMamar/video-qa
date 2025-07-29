7fa51df7afff36206ee0e25a5bac61323bab9aef
fadel <fadel.seydou@gmail.com>
2025-07-29 17:40:20 +0200

    update

--- launch_vlm_endpoint.bat
+++ launch_vlm_endpoint.bat
@@ -3,8 +3,12 @@
 
 @REM available quantizations q8_0 f16 q4_k_m
 
-start %LLAMA_SERVER_PATH% -hf %MODEL_NAME% ^
-     --port %PORT% --ctx-size %CTX_SIZE%
+call load_env.bat
+
+call %LLAMA_SERVER_PATH% -hf %MODEL_NAME% ^
+     --port %VLM_PORT% --ctx-size %CTX_SIZE%
+
+call pause
 
 @REM start D:\workspace\llama-cpp\llama-server.exe -hf ggml-org/Qwen3-1.7B-GGUF:q8_0 ^
 @REM      --port 8001 
